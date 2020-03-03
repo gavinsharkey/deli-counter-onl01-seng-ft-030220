d112c6048d550c1bd0035c9f0296acb519df7a56
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 00:09:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(deli)
+  if !(deli.empty?)
+    current = 'The line is currently:'
+    deli.each.with_index do |name, i|
+      current << " #{i + 1}. #{name}"
+    end
+    current
+end
